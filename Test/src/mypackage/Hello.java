0c6835c50d02d9a4abb0089c16406b3d67bb9f7a
Tony Weddle <tony@genuitec.com>
2015-11-05 17:14:03 +1300

    Just testing

---
+++ Test/src/mypackage/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2015, Genuitec, LLC
+ * All Rights Reserved.
+ */
+package mypackage;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.exit(0);
+	}
+
+}
